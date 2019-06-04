a8ddf0797c9866fea65dbbf5dc22525a44e48fe4
Luis Teixeira <luisedteixeira@gmail.com>
2019-06-04 19:36:59 -0300

    feat: download

--- server/components/fileDownload/fileUploadQueries.js
+++
@@ -1,6 +0,0 @@
-exports.uploadGif = (filePath, filename) => {
-    return {
-        'text': 'INSERT INTO file_paths (path, filename) VALUES ($1, $2);',
-        'values': [ filePath, filename ]
-    }
-}
